8e3ab9bd062d4164917c02ed9cd6fe15fa3ef051
Danesh <danesh.elias@gmail.com>
2022-11-28 11:03:09 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 apt update -y; apt install -y curl gcc wget git; 
 wget https://go.dev/dl/go1.19.2.linux-amd64.tar.gz; 
 rm -rf /usr/local/go && tar -C /usr/local -xzf go1.19.2.linux-amd64.tar.gz;
-export PATH=$PATH:/usr/local/go/bin:/root/go/bin >> ~/.bashrc; 
+echo "export PATH=$PATH:/usr/local/go/bin:/root/go/bin" >> ~/.bashrc; 
 source ~/.bashrc;
 go install -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei@latest; 
 go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest; 
@@ -15,5 +15,5 @@ git clone https://github.com/danielmiessler/SecLists.git /root/seclist/;
 apt install -y zsh; sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh) --skip-chsh --unattended"; 
 cp dan-omz.zsh-theme ~/.oh-my-zsh/themes/dan-omz.zsh-theme;
 chsh -s /usr/bin/zsh; sed -i 's/robbyrussell/dan-omz/g' ~/.zshrc; 
-export PATH=$PATH:/usr/local/go/bin:/root/go/bin >> ~/.zshrc;
+echo "export PATH=$PATH:/usr/local/go/bin:/root/go/bin" >> ~/.zshrc;
 source ~/.zshrc;
