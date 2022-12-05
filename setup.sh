7ef50eb367815e587b95a0f0aaa98d20e6bce210
Danesh <danesh.elias@gmail.com>
2022-12-05 16:41:43 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-apt update -y; apt install -y curl gcc wget git; 
+apt update -y; apt install -y curl gcc wget git libpcap-dev; 
 wget https://go.dev/dl/go1.19.2.linux-amd64.tar.gz; 
 rm -rf /usr/local/go && tar -C /usr/local -xzf go1.19.2.linux-amd64.tar.gz;
 echo "export PATH=$PATH:/usr/local/go/bin:/root/go/bin" >> ~/.bashrc; 
@@ -10,6 +10,7 @@ go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest;
 go install github.com/tomnomnom/meg@latest; 
 go install -v github.com/projectdiscovery/uncover/cmd/uncover@latest;
 go install github.com/OJ/gobuster/v3@latest;
+go install -v github.com/projectdiscovery/naabu/v2/cmd/naabu@latest;
 go install github.com/ffuf/ffuf@latest;
 git clone https://github.com/danielmiessler/SecLists.git /root/seclist/;
 apt install -y zsh; sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh) --skip-chsh --unattended"; 
